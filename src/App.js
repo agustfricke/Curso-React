35e4bff366c9bda853d9219d3e716bd59db257cf
Agust <agustfricke@protonmail.com>
2023-03-14 19:55:21 -0500

    state objects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
+
+  const[user, setUser] = useState([
+    'Agustin',
+    'Fricke',
+   '23'
+  ])
+
+  const updateUser = () => {
+    setUser(previoState => {
+      return {...previoState, edad: '34'}
+    })
+  }
+
   return (
     <div>
-      <button>No hago nada!</button>
+      <p>name: {user.name}</p>
+      <p>lastname: {user.lastName}</p>
+      <p>edad: {user.edad}</p>
+      <button
+        type="button"
+        onClick={updateUser}
+      >Cambiar edad a 34</button>
     </div>
   );
 }
+
 
