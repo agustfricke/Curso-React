5946308b1f2558824a63fa5d32f26177516c68e8
Agust <agustfricke@protonmail.com>
2023-03-14 22:11:32 -0500

    use content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState, createContext, useContext } from "react";
+
+
+const UserContext = createContext();
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
+  const [user, setUser] = useState("Jesse Hall");
 
-  useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]); // <- add the count variable here
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 />
+    </UserContext.Provider>
+  );
+}
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
 
   return (
     <>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
-      <p>Calculation: {calculation}</p>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
     </>
   );
-
 }
-
-
